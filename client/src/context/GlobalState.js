43ffcac0b79f84569d54757c3624e5d6e3586771
Paras Nagpal <paras.nagpal743@gmail.com>
2023-08-14 11:01:09 +0530

    optimized perfomance

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,89 +1,89 @@
-import React , {createContext , useReducer} from 'react';
-import reducer from './AppReducer' ;
-import axios from 'axios'
-const URL = 'https://hisaab-0ejx.onrender.com' ;
-// Intial State 
+import React, { createContext, useReducer, useEffect } from 'react';
+import reducer from './AppReducer';
+import axios from 'axios';
+
+const API_BASE_URL = 'https://hisaab-0ejx.onrender.com/api/v1';
+
 const initialState = {
-    transactions : [] ,
-    error : null ,
-    loading : true 
-}
+  transactions: [],
+  error: null,
+  loading: true,
+};
 
-// Create Context 
-export const GlobalContext = createContext(initialState) ;
+export const GlobalContext = createContext(initialState);
 
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-// Provider component 
-export const GlobalProvider = ({children}) =>{
-    const [state , dispatch] = useReducer(reducer , initialState) ;
-    // Actions 
-    const getTransactions =async()=>{
-          try{
-            dispatch({type : 'REQUEST'});
-            const res = await axios.get(`${URL}/api/v1/transactions`);
-            dispatch({
-                type : 'GET_TRANSACTIONS' ,
-                payload : res.data.data 
-            })
-          }catch(err){
-             dispatch({
-                type : 'TRANSACTION_ERROR' ,
-                payload : err.response.data.error 
-             });
-          }
+  // Actions
+  const handleError = (error) => {
+    dispatch({
+      type: 'TRANSACTION_ERROR',
+      payload: error.response ? error.response.data.error : 'An error occurred',
+    });
+  };
+
+  const getTransactions = async () => {
+    try {
+      dispatch({ type: 'REQUEST' });
+      const res = await axios.get(`${API_BASE_URL}/transactions`);
+      dispatch({
+        type: 'GET_TRANSACTIONS',
+        payload: res.data.data,
+      });
+    } catch (err) {
+      handleError(err);
     }
+  };
 
-
-
-    const deleteTransaction =async(id)=> {
-        dispatch({type : 'REQUEST'})
-        try{
-           await axios.delete(`${URL}/api/v1/transactions/${id}`);
-           dispatch({
-             type : 'DELETE_TRANSACTION' ,
-             payload : id 
-           })
-        }catch(err){
-           dispatch({
-            type : 'TRANSACTON_ERROR' ,
-            payload : err.response.data.error 
-           })
-        }
+  const deleteTransaction = async (id) => {
+    try {
+      dispatch({ type: 'REQUEST' });
+      await axios.delete(`${API_BASE_URL}/transactions/${id}`);
+      dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id,
+      });
+    } catch (err) {
+      handleError(err);
     }
-  
-    const addTransaction=async(transaction)=>{
-        dispatch({type : 'REQUEST'})
-        const config = {
-            headers : {
-                'Content-Type' : 'application/json'
-            }
-        }
-        try{
-           const res = await axios.post(`${URL}/api/v1/transactions`,transaction,config)
-           dispatch({
-            type : 'ADD_TRANSACTION' ,
-            payload : res.data.data  
-        })
-        }catch(err){
-            dispatch({
-                type : 'TRANSACTON_ERROR' ,
-                payload : err.response.data.error 
-               })
-        }
+  };
+
+  const addTransaction = async (transaction) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      dispatch({ type: 'REQUEST' });
+      const res = await axios.post(`${API_BASE_URL}/transactions`, transaction, config);
+      dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: res.data.data,
+      });
+    } catch (err) {
+      handleError(err);
     }
+  };
 
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Fetch transactions when the component mounts
 
-    return (
-        <GlobalContext.Provider value={{
-            transactions : state.transactions,
-            error : state.error ,
-            loading : state.loading ,
-            deleteTransaction ,
-            addTransaction , 
-            getTransactions 
-        }}>
-            {children}
-        </GlobalContext.Provider>
-    )
-
-}
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        deleteTransaction,
+        addTransaction,
+        getTransactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
